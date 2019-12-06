cfdabf24bf368eff9fe176afded1bd99d8779f2c
Alberto Fuentes <caf004m@email.wal-mart.com>
2019-12-06 09:37:09 -0600

    dynamic form

--- src/app/pages/order/order.page.ts
+++ src/app/pages/order/order.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {AlertController, LoadingController} from '@ionic/angular';
 import {FirebaseService} from '../../services/firebase/firebase.service';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -16,12 +16,12 @@ export class OrderPage implements OnInit {
   itemForm: FormGroup;
   loading: any;
 
+  items: FormArray;
   arrayItems: {
-    id: number;
-    pieces: number;
-    width: number;
-    height: number;
-    total: number;
+    pieces: any;
+    width: any;
+    height: any;
+    total: any;
   }[];
 
   constructor(
@@ -35,24 +35,28 @@ export class OrderPage implements OnInit {
 
   ngOnInit() {
     this.arrayItems = [];
-    this.itemForm = this.formBuilder.group({
-      pieces: new FormControl(''),
-      width: new FormControl(''),
-      height: new FormControl('' ),
-      total: new FormControl('' )
-    });
 
     this.form = this.formBuilder.group({
       date: new FormControl(''),
       client: new FormControl(''),
       notes: new FormControl(''),
-      items: this.formBuilder.array([])
+      items: this.formBuilder.array([this.createItem()])
     });
 
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id) {
       this.getClient(id);
     }
+  }
+
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+      pieces: new FormControl(''),
+      width: new FormControl(''),
+      height: new FormControl('' ),
+      areaTotal: new FormControl('' ),
+      areaUnit: new FormControl('' )
+    });
   }
 
   async presentAlert(header, message) {
@@ -125,21 +129,22 @@ export class OrderPage implements OnInit {
     this.presentAlert('Cliente', err.toString());
   }
 
-  addItem() {
-    const items = this.form.controls.item.value;
-    this.arrayItems.push({
-      id: 1,
-      pieces: 11,
-      width: 1,
-      height: 1,
-      total: 2
-    });
-    items.push(this.formBuilder.control(false));
-  }
   removeItem() {
     const items = this.form.controls.item.value;
     this.arrayItems.pop();
     items.removeAt(items.length - 1);
+  }
+
+  addItemForm() {
+    this.items = this.form.get('items') as FormArray;
+    this.items.push(this.createItem());
+  }
+
+  updateTotal(item) {
+    item.controls.areaTotal.patchValue(item.value.width * item.value.height * item.value.pieces);
+    item.controls.areaUnit.patchValue(item.value.width * item.value.height);
+    
+    console.log(this.form);
   }
 
 }
